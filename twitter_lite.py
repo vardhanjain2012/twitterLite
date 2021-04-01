c67bccdd084c8b028a32682631c1396bfee8388a
Ashish R Nair <ashish@Ashishs-MacBook-Pro.local>
2021-04-01 15:54:47 +0530

    follower-following and some formatting

--- twitter_lite.py
+++ twitter_lite.py
@@ -76,17 +76,88 @@ def profile(name):
     con.commit()
     cur.close()
 
-    # followers ='''SELECT u.name, count(*) as count
-    #                         FROM user_map as u1, user_list_w_newid as u_dash1, graph_cb, 
-    #                         WHERE u.oldid = user_list_w_newid.oldid AND user_list_w_newid.newid = 
-    #                         GROUP BY u.name
-    #                         ORDER BY n_replies desc
-    #                         LIMIT 10'''
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    
+    followers_q = '''
+                SELECT user_map.name as follower_name
+                FROM 
+                    (
+                    SELECT user_list_w_newid.oldid AS follower_oldid
+                    FROM
+                        (
+                        SELECT graph_cb.follower_newid AS follower_newid
+                        FROM
+                            (
+                            SELECT user_list_w_newid.newid as newid
+                            FROM
+                                (
+                                SELECT oldid 
+                                FROM user_map
+                                WHERE name = \'%s\'
+                                ) AS foo,
+                                user_list_w_newid
+                            WHERE foo.oldid = user_list_w_newid.oldid
+                            ) AS bar,
+                            graph_cb
+                        WHERE graph_cb.following_newid = bar.newid
+                        ) as foobar1,
+                        user_list_w_newid
+                    WHERE foobar1.follower_newid = user_list_w_newid.newid
+                    ) AS foobar2,
+                    user_map
+                WHERE user_map.oldid = follower_oldid
+                ;
+                ''' %(name)
+
+    cur.execute(followers_q)
+
+    followers = cur.fetchall()
+
+    con.commit()
+    cur.close()
+
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    followings_q = '''
+                SELECT user_map.name as following_name
+                FROM 
+                    (
+                    SELECT user_list_w_newid.oldid AS following_oldid
+                    FROM
+                        (
+                        SELECT graph_cb.following_newid AS following_newid
+                        FROM
+                            (
+                            SELECT user_list_w_newid.newid as newid
+                            FROM
+                                (
+                                SELECT oldid 
+                                FROM user_map
+                                WHERE name = \'%s\'
+                                ) AS foo,
+                                user_list_w_newid
+                            WHERE foo.oldid = user_list_w_newid.oldid
+                            ) AS bar,
+                            graph_cb
+                        WHERE graph_cb.follower_newid = bar.newid
+                        ) as foobar1,
+                        user_list_w_newid
+                    WHERE foobar1.following_newid = user_list_w_newid.newid
+                    ) AS foobar2,
+                    user_map
+                WHERE user_map.oldid = following_oldid
+                ;
+                ''' %(name)
+
+    cur.execute(followings_q)
+
+    followings = cur.fetchall()
+
+    con.commit()
+    cur.close()
 
     con.close()
-    return render_template("profile.html", name = name, tweets = tweets)
+    return render_template("profile.html", name = name, tweets = tweets, followers = followers, follower_count = len(followers), followings = followings, following_count = len(followings))
 
 
 if __name__ == "__main__":
