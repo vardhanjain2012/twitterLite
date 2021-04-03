b8b6f2827324964fd74768ac8232040c1a4dfcd3
Shivam Bansal <cs1170376@iitd.ac.in>
2021-04-03 19:24:53 +0530

    explore added

--- twitter_lite.py
+++ twitter_lite.py
@@ -25,11 +25,132 @@ def hello_world():
 
 @app.route('/feed/<username>')
 def feed(username):
-	cur = con.cursor()
-	cur.execute("SELECT * FROM tweets_withoutwords WHERE user_name=%s", [username])
-	items = cur.fetchall()
-	cur.close()
-	return render_template("feed.html", users=items)
+	con = psycopg2.connect(dbname='twitter_lite', user='postgres', host='localhost', password='490023')
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    tweets_q = '''SELECT * 
+				FROM (SELECT user_map.name as follower_name
+					FROM 
+						(
+						SELECT user_list_w_newid.oldid AS follower_oldid
+						FROM
+							(
+							SELECT graph_cb.follower_newid AS follower_newid
+							FROM
+								(
+								SELECT user_list_w_newid.newid as newid
+								FROM
+									(
+									SELECT oldid 
+									FROM user_map
+									WHERE name = \'%s\'
+									) AS foo,
+									user_list_w_newid
+								WHERE foo.oldid = user_list_w_newid.oldid
+								) AS bar,
+								graph_cb
+							WHERE graph_cb.following_newid = bar.newid
+							) as foobar1,
+							user_list_w_newid
+						WHERE foobar1.follower_newid = user_list_w_newid.newid
+						) AS foobar2,
+						user_map
+					WHERE user_map.oldid = follower_oldid) AS p, 
+				tweets_withoutwords AS t
+                WHERE t.user_name = follower_name
+                ORDER BY t.date desc, t.time desc
+                '''%(name)
+    
+    cur.execute(tweets_q)
+
+    tweets = cur.fetchall()
+
+    con.commit()
+    cur.close()
+
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    followers_q = '''
+                SELECT user_map.name as follower_name
+                FROM 
+                    (
+                    SELECT user_list_w_newid.oldid AS follower_oldid
+                    FROM
+                        (
+                        SELECT graph_cb.follower_newid AS follower_newid
+                        FROM
+                            (
+                            SELECT user_list_w_newid.newid as newid
+                            FROM
+                                (
+                                SELECT oldid 
+                                FROM user_map
+                                WHERE name = \'%s\'
+                                ) AS foo,
+                                user_list_w_newid
+                            WHERE foo.oldid = user_list_w_newid.oldid
+                            ) AS bar,
+                            graph_cb
+                        WHERE graph_cb.following_newid = bar.newid
+                        ) as foobar1,
+                        user_list_w_newid
+                    WHERE foobar1.follower_newid = user_list_w_newid.newid
+                    ) AS foobar2,
+                    user_map
+                WHERE user_map.oldid = follower_oldid
+                ;
+                ''' %(name)
+
+    cur.execute(followers_q)
+
+    followers = cur.fetchall()
+
+    con.commit()
+    cur.close()
+
+    cur = con.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    followings_q = '''
+                SELECT user_map.name as following_name
+                FROM 
+                    (
+                    SELECT user_list_w_newid.oldid AS following_oldid
+                    FROM
+                        (
+                        SELECT graph_cb.following_newid AS following_newid
+                        FROM
+                            (
+                            SELECT user_list_w_newid.newid as newid
+                            FROM
+                                (
+                                SELECT oldid 
+                                FROM user_map
+                                WHERE name = \'%s\'
+                                ) AS foo,
+                                user_list_w_newid
+                            WHERE foo.oldid = user_list_w_newid.oldid
+                            ) AS bar,
+                            graph_cb
+                        WHERE graph_cb.follower_newid = bar.newid
+                        ) as foobar1,
+                        user_list_w_newid
+                    WHERE foobar1.following_newid = user_list_w_newid.newid
+                    ) AS foobar2,
+                    user_map
+                WHERE user_map.oldid = following_oldid
+                ;
+                ''' %(name)
+
+    cur.execute(followings_q)
+
+    followings = cur.fetchall()
+
+    con.commit()
+    cur.close()
+
+    con.close()
+    return render_template("feed.html", name = name, tweets = tweets, followers = followers, follower_count = len(followers), followings = followings, following_count = len(followings))
+
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data_signup():
